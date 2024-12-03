87e1f403166b8b051e1faf42755420b0da7dbd0f
Halvor Linder <halvor_lh@hotmail.no>
2024-12-03 11:46:28 +0100

    swap rvsdgptr for llvmptr

--- include/RVSDG/RVSDGOps.h
+++ include/RVSDG/RVSDGOps.h
@@ -3,6 +3,7 @@
 #include "mlir/IR/OpDefinition.h"
 #include "mlir/IR/BuiltinTypes.h"
 #include "mlir/Dialect/Func/IR/FuncOps.h"
+#include "mlir/Dialect/LLVMIR/LLVMTypes.h"
 
 #include "mlir/Interfaces/CallInterfaces.h"
 #include "mlir/Interfaces/LoopLikeInterface.h"
